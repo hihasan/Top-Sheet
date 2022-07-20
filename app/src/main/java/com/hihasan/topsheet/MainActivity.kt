6d3792b6de3cb158cffc4a1376ab1d79df7d7976
hihasan <hihasan07@gmail.com>
2022-07-21 01:54:33 +0600

    Top Sheet Dialog

--- app/src/main/java/com/hihasan/topsheet/MainActivity.kt
+++ app/src/main/java/com/hihasan/topsheet/MainActivity.kt
@@ -2,10 +2,27 @@ package com.hihasan.topsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.hihasan.topsheet.databinding.DemoLayoutBinding
 
 class MainActivity : AppCompatActivity() {
+    private var demoLayoutBinding : DemoLayoutBinding ?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        demoLayoutBinding = DataBindingUtil.setContentView(this, R.layout.demo_layout)
+        
+        initViews()
+    }
+
+    private fun initViews() {
+        TopSheetDialog(applicationContext, R.style.TopSheet_DialogAnimation).apply {
+            // Required to have the top-down animation when the app starts showing / dismissin it.
+            window?.attributes?.windowAnimations = R.style.TopSheet_DialogAnimation
+            setContentView(demoLayoutBinding!!.root , ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        }.show()
     }
 }
